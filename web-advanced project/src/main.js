7305fba5af1a52056bfa7391ea3a1b8b3ac8f136
sounya <sounya-abattouy@live.be>
2026-04-02 10:57:58 +0200

    git add .

---
+++ web-advanced project/src/main.js
@@ -0,0 +1,7 @@
+// vite setup with vanilla without framework https://www.youtube.com/watch?v=iUXt3EtXRaw
+
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite 👋</h1>
+`
