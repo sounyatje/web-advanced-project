186bac0fe7fb36da641ae0ace3bb2de5cad2158d
sounya <sounya-abattouy@live.be>
2026-04-02 12:59:23 +0200

    git push

--- web-advanced-project/src/main.js
+++ web-advanced-project/src/main.js
@@ -1,7 +1,7 @@
 // vite setup with vanilla without framework https://www.youtube.com/watch?v=iUXt3EtXRaw
 
-import './style.css'
+import './style.scss'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite 👋</h1>
+  <h1>Attach on titan API</h1>
 `
