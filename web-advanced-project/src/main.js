3e0b1d494c6da996b1717a6939623dd250d21f9a
Sounyatje <sounya-abattouy@live.be>
2026-06-07 00:17:53 +0200

    javascript

--- web-advanced-project/src/main.js
+++ web-advanced-project/src/main.js
@@ -2,3 +2,24 @@
 
 import './style.scss'
 
+let items = document.querySelectorAll('.item')
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    items.forEach(i => i.classList.remove('active'))
+    item.classList.add('active')
+    console.log('geklikt:', item);
+  });
+
+  item.addEventListener('mouseenter', () => {
+    item.classList.add('hover')
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.classList.remove('hover')
+  });
+});
+
+let arrowleft = document.querySelector('.arrow left')
+    arrowleft.addEventListener('click', () => {
+  img.src = './src/images/controlLR.png';
+});
